7c7b48479524d85a29430c2ce4a6c24dc031e416
jebitama <jebitama@gmail.com>
2026-02-28 18:00:25 +0800

    refactor: use commonjs for api directory to support extensionless imports natively in Node

--- api/jobs/notify-admin.ts
+++ api/jobs/notify-admin.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { verifySignatureAppRouter } from '@upstash/qstash/dist/nextjs';
+
 
 async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
